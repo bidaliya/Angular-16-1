3f923182adb85cb1b55edafb2ef107577e64f30c
bidalliya <bidaliyatarun427@gmail.com>
2023-11-01 21:47:03 +0530

    added bootstrap 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+
+  password :string = '';
+  wantLetter:boolean = false;
+  wantNumbers:boolean = false;
+  wantSymbols:boolean = false;
+  length : number = 0;
+  onChangeLength(event:Event){
+    var target = event.target as HTMLInputElement
+    this.length = parseInt(target.value)
+  }
+  useLetter(){
+    this.wantLetter = !this.wantLetter;
+    console.log('wantLetter',this.wantLetter)
+  }
+  useSymbols(){
+    this.wantSymbols = !this.wantSymbols;
+    console.log('wantSymbols',this.wantSymbols)
+
+  }
+  useNumbers(){
+    this.wantNumbers = !this.wantNumbers;
+    console.log('wantNumbers',this.wantNumbers);
+
+  }
+  generate(){
+    // if(!this.wantLetter && !this.wantNumbers && !this.wantSymbols){
+      
+    //   return alert('Please select atleast 1 checkbox');
+    // }
+    // if(this.length <0){
+    //   return alert('length should be greater than or equals to 0')
+    // }
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '1234567890';
+    var symbols = '!@#$%^&*()';
+
+    var completePossiblePassword :any = '';
+    if(this.wantLetter){
+      completePossiblePassword += letters;
+    }
+    if(this.wantNumbers){
+      completePossiblePassword += numbers;
+    }
+    if(this.wantSymbols){
+      completePossiblePassword += symbols;
+    }
+
+    var generatedPassword = "";
+    for (let i = 0; i < this.length; i++) {
+      var index = Math.floor(Math.random() * completePossiblePassword.length);
+      generatedPassword += completePossiblePassword[index];
+    }
+    this.password = generatedPassword;
+  }
+  getPassword(){
+    return this.password;
+  }
 }
